3696e9fd578d95b170fc92dff21ee50e90b5d6b0
Momina Umar <m61763193@gmail.com>
2025-07-15 10:55:02 -0700

    your message here

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer className="mt-20 border-t py-10 border-neutral-700">
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         <div>
-          <h3 className="text-md font-semibold mb-4">Resources</h3>
+          <h3 className="text-md font-semibold mb-4">Resource</h3>
           <ul className="space-y-2">
             {resourcesLinks.map((link, index) => (
               <li key={index}>
